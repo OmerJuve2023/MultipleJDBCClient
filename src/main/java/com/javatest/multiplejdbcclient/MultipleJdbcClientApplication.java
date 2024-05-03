01f37b1f81a14b8d768680e14a4d79884480b02b
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-05-02 19:33:14 -0500

    Initial commit

---
+++ src/main/java/com/javatest/multiplejdbcclient/MultipleJdbcClientApplication.java
@@ -0,0 +1,13 @@
+package com.javatest.multiplejdbcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleJdbcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleJdbcClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatest/multiplejdbcclient/MultipleJdbcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatest.multiplejdbcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleJdbcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
